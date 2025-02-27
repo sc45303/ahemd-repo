aa90f2e295ebbd7dbe4bea79f5ed81fa3afdef60
suhail <mygamingchanal786@gmail.com>
2025-02-27 10:55:37 +0500

    com

--- practise/app.py
+++ practise/app.py
@@ -32,4 +32,4 @@
 # write a program to format the following letter using escape sequence characters.
 
 letter = "Dear Suhail, \n\tThis python course is nice. \nThanks!"
-print(letter)
+print(letter) 
